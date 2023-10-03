f4e230126249940a7c422d0445e25b9668e08b67
John Shaffer <john@insilica.co>
2023-10-03 16:48:42 -0500

    Get all pages of org repo list

--- components/github/src/biobricks/github/ifc.clj
+++ components/github/src/biobricks/github/ifc.clj
@@ -3,19 +3,30 @@
             [clojure.java.io :as io]
             [hato.client :as hc]))
 
-(defn get-url [url]
+(defn get-url [url & [opts]]
   (let [response (hc/get
                   url
-                  {:headers
-                   {"Accept" "application/vnd.github+json"
-                    "User-Agent" "Hato"
-                    "X-GitHub-Api-Version" "2022-11-28"}
-                   :as :stream})]
+                  (merge
+                   {:headers
+                    {"Accept" "application/vnd.github+json"
+                     "Authorization" "Bearer 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"
+                     "User-Agent" "Hato"
+                     "X-GitHub-Api-Version" "2022-11-28"}
+                    :as :stream}
+                   opts))]
     (-> response :body io/reader (json/read {:key-fn keyword}))))
 
 (defn list-org-repos
   "Lists repositories for the specified organization.
 
    https://docs.github.com/en/free-pro-team@latest/rest/repos/repos?apiVersion=2022-11-28#list-organization-repositories"
-  [org-name]
-  (get-url (str "https://api.github.com/orgs/" org-name "/repos")))
+  [org-name & [page-num]]
+  (lazy-seq
+   (let [page-num (or page-num 1)
+         per-page 30
+         results (get-url (str "https://api.github.com/orgs/" org-name "/repos")
+                          {:query-params {"page" page-num "per_page" per-page}})]
+     (concat
+      results
+      (when (= per-page (count results))
+        (list-org-repos org-name (inc page-num)))))))

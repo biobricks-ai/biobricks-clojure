8ad178b513f4646ef7313c01809bea118d7daa41
John Shaffer <john@insilica.co>
2024-01-17 16:55:03 -0600

    Print errors that occur in ring handlers

--- bases/web-ui/src/biobricks/web_ui/api/ring.clj
+++ bases/web-ui/src/biobricks/web_ui/api/ring.clj
@@ -2,6 +2,7 @@
   (:require [biobricks.web-ui.api.routes :as routes]
             [biobricks.web-ui.impl.badge :as badge]
             [biobricks.web-ui.impl.webhook :as webhook]
+            [clojure.stacktrace :as st]
             [reitit.core :as rr]
             [reitit.ring :as rring]))
 
@@ -13,12 +14,18 @@
 (defn wrap-routes [f]
   (let [router (rring/router routes/routes)]
     (fn [request]
-      (let [match (rr/match-by-path router (:uri request))
-            handler (-> match :data :name handlers)
-            request (assoc request :match match)]
-        (or
-          (when handler (handler request))
-          (f request))))))
+      (try
+        (let [match (rr/match-by-path router (:uri request))
+              handler (-> match :data :name handlers)
+              request (assoc request :match match)]
+          (or
+            (when handler (handler request))
+            (f request)))
+        (catch Throwable e
+          (print "Error in ring handler")
+          (st/print-throwable e)
+          (st/print-cause-trace e)
+          (throw e))))))
 
 (defn wrap-instance [f instance]
   (fn [request]
